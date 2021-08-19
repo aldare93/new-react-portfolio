68c89f1b1b40be7b94cda566c3cb73f6a9c45d24
Alex DaRe <alexdare93@gmail.com>
2021-08-19 03:25:32 -0700

    messing with the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div classNameName="footer">
-      <footer className="py-5 fixed-bottom">
+      <footer className="py-5">
         <div className="container">
           <p className="m-0 text-center text-white">
             Copyright &copy; Alex DaRe 2020
