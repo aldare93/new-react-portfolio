d3f08c552bcd14ed7315c4521fcef26bf5b3d936
Alex DaRe <alexdare93@gmail.com>
2021-08-19 01:17:43 -0700

    portfolio page started

---
+++ src/Pages/Portfolio.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Portfolio() {
+  return (
+    <div className="container homepage">
+        <div className="row">
+            <div className="col">
+                <h1>Portfolio</h1>     
+             </div>
+        </div>
+    </div>
+  );
+}
+
+export default Portfolio;
