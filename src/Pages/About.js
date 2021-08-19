851940c441a6bf6a23dd7df9c5cf63a6b797cf6b
Alex DaRe <alexdare93@gmail.com>
2021-08-19 01:09:48 -0700

    about page added

---
+++ src/Pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="container homepage">
+        <div className="row">
+            <div class="col">
+                <h1>About me</h1>
+                <br />
+                <p>I'm originally from Carlsbad, California, a city located in northern San Diego county. I attended college at the University of California, Rivereside (UCR) from 2012 - 2016 and studied economics.</p>
+                <p>I began my professional career at Donnelley Financial Solutions in early 2017. My role at Donnelley was a Financial Services Account Specialist. As a Financial Services Account Specialist, I was a primary point of contact for clients and oversaw the creation and filing of all types of securities related documents going to the SEC, ranging from small compliance documents to initial public offerings. I worked at DFIN until May of 2019 at both their San Diego and Irvine offices.</p>
+                <p>In July of 2019 I started work at Atlassian in San Francisco, CA as a web producer.</p>
+             </div>
+        </div>
+    </div>
+  );
+}
+
+export default About;
