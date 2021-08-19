52a66b3ef137c4ed48d369203ddd3eca19990068
Alex DaRe <alexdare93@gmail.com>
2021-08-19 01:04:27 -0700

    default exports added for components and Pages

---
+++ src/Pages/index.js
@@ -0,0 +1,5 @@
+export { default as About } from "../Pages/About";
+export { default as Home } from "../Pages/Home";
+export { default as Contact } from "../Pages/Contact";
+export { default as Portfolio } from "../Pages/Portfolio"
+export { default as Resume } from "../Pages/Resume"
