843d83a444a1308a8b8a32d5d758b1ffeab9f22b
Alex DaRe <alexdare93@gmail.com>
2021-08-19 01:10:23 -0700

    contact page added

---
+++ src/Pages/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="container homepage">
+        <div className="row">
+            <div className="col">
+                <h1>Contact</h1>
+                <br />
+                <p>If you'd like to discuss my projects or have any questions, you can email me at <a href="mailto:alexdare93@gmail.com">alexdare93@gmail.com</a>. Additionally, you can connect with me on <a href="https://www.linkedin.com/in/darealex/">Linkedin</a>.</p>
+             </div>
+        </div>
+    </div>
+  );
+}
+
+export default Contact;
